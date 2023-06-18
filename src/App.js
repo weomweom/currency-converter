6fe020119d38ef5d7961060054d26fa3006e8ced
weomweom <16.homiaq.16@gmail.com>
2023-06-18 11:02:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Block from "./Block";
 import ACCESS_KEY from "./ACCESS_KEY";
 
 function App() {
-  //useRef instead of useState?
+  //add a drop-down list!!!
+  const rates = useRef({});
+  /*
   const [rates, setRates]= useState({
     'USD': 1,
     'EUR': 0.91,
     'PLN': 4.6
     //data for testing
   });
-/*
+  */
+
   useEffect(() => {
     fetch(`http://data.fixer.io/api/latest?access_key=${ACCESS_KEY}`)
       .then(response => response.json())
       .then(data => {
-        setRates(data.rates);
+        rates.current = data.rates;
+        fromCalcValue(fromValueCurrency);
       })
       .catch(error => {
         console.error(error);
       });
   }, []);
-*/
+
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("PLN");
   const [fromValueCurrency, setFromValueCurrency] = useState(1);
@@ -43,15 +47,15 @@ function App() {
   };
 
   const fromCalcValue = (value) => {
-    const price = value / rates[fromCurrency];
-    const result = price * rates[toCurrency];
+    const price = value / rates.current[fromCurrency];
+    const result = price * rates.current[toCurrency];
     setFromValueCurrency(value);
     setToValueCurrency(Math.round(result*1000)/1000);
   };
 
   const toCalcValue = (value) => {
-    const price = value / rates[toCurrency];
-    const result = price * rates[fromCurrency];
+    const price = value / rates.current[toCurrency];
+    const result = price * rates.current[fromCurrency];
     setToValueCurrency(value);
     setFromValueCurrency(Math.round(result*1000)/1000);
   };
